410c8c82882aa22b21044eb3234a7076f4483478
Rajanith Bandara <RajanithBandara123@outlook.com>
2024-03-18 18:00:17 +0530

    chart created

--- WindowsAppProject/Apps/usercontrol_studentdash/testchart.Designer.cs
+++ WindowsAppProject/Apps/usercontrol_studentdash/testchart.Designer.cs
@@ -28,19 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
             this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(38, 32);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(831, 591);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
             // 
             // testchart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.plotView1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "testchart";
-            this.Size = new System.Drawing.Size(856, 536);
+            this.Size = new System.Drawing.Size(963, 670);
             this.Load += new System.EventHandler(this.testchart_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
     }
 }
--- WindowsAppProject/Apps/usercontrol_studentdash/testchart.cs
+++ WindowsAppProject/Apps/usercontrol_studentdash/testchart.cs
@@ -18,6 +18,23 @@ namespace WindowsAppProject.Apps.usercontrol_studentdash
         public testchart()
         {
             InitializeComponent();
+            InitializePlot();
+        }
+
+        private void InitializePlot()
+        {
+            var plotModel = new PlotModel { Title = "Simple Line Plot" };
+
+            var series = new LineSeries();
+            Random rand = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                series.Points.Add(new DataPoint(i, rand.Next(1, 100)));
+            }
+
+            plotModel.Series.Add(series);
+            plotView1.Model = plotModel;
+            
         }
 
         private void testchart_Load(object sender, EventArgs e)
