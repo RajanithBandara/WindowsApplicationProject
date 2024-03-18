ccc2c7548870eb1ee5a108ee41ad5e03687e4d7b
wethmi <135610348+wethmi@users.noreply.github.com>
2024-03-18 17:50:47 +0530

    create a test chart usercontrol

---
+++ WindowsAppProject/Apps/usercontrol_studentdash/testchart.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WindowsAppProject.Apps.usercontrol_studentdash
+{
+    partial class testchart
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // testchart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "testchart";
+            this.Size = new System.Drawing.Size(856, 536);
+            this.Load += new System.EventHandler(this.testchart_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ WindowsAppProject/Apps/usercontrol_studentdash/testchart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OxyPlot;
+using OxyPlot.WindowsForms;
+using OxyPlot.Series;
+
+namespace WindowsAppProject.Apps.usercontrol_studentdash
+{
+    public partial class testchart : UserControl
+    {
+        public testchart()
+        {
+            InitializeComponent();
+        }
+
+        private void testchart_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
