8be3a45eacc569713712474c94401647dd1e81ec
Rajanith Bandara <RajanithBandara123@outlook.com>
2024-03-23 19:03:35 +0530

    database connection related some logics done

--- WindowsAppProject/Apps/usercontrol_studentdash/testchart.Designer.cs
+++ WindowsAppProject/Apps/usercontrol_studentdash/testchart.Designer.cs
@@ -28,29 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // plotView1
+            // panel1
             // 
-            this.plotView1.Location = new System.Drawing.Point(60, 26);
-            this.plotView1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.plotView1.Name = "plotView1";
-            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this.plotView1.Size = new System.Drawing.Size(739, 473);
-            this.plotView1.TabIndex = 0;
-            this.plotView1.Text = "plotView1";
-            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
-            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
-            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.panel1.Location = new System.Drawing.Point(37, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(894, 619);
+            this.panel1.TabIndex = 0;
             // 
             // testchart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.plotView1);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "testchart";
-            this.Size = new System.Drawing.Size(856, 536);
+            this.Size = new System.Drawing.Size(963, 670);
             this.Load += new System.EventHandler(this.testchart_Load);
             this.ResumeLayout(false);
 
@@ -58,6 +53,6 @@
 
         #endregion
 
-        private OxyPlot.WindowsForms.PlotView plotView1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- WindowsAppProject/Apps/usercontrol_studentdash/testchart.cs
+++ WindowsAppProject/Apps/usercontrol_studentdash/testchart.cs
@@ -15,51 +15,13 @@ namespace WindowsAppProject.Apps.usercontrol_studentdash
 {
     public partial class testchart : UserControl
     {
-        private PlotModel plotModel;
-        private Label valueLabe1;
-        private LineSeries series;
         public testchart()
         {
             InitializeComponent();
-            InitializePlot();
-        }
-
-        private void InitializePlot()
-        {
-            var plotModel = new PlotModel { Title = "Simple Line Plot" };
-
-            var series = new LineSeries();
-            Random rand = new Random();
-            for (int i = 0; i < 11; i++)
-            {
-                series.Points.Add(new DataPoint(i, rand.Next(1, 100)));
-            }
-
-            plotModel.Series.Add(series);
-            plotView1.Model = plotModel;
-            
-        }
-
-        private void Initializecontrols()
-        {
-            valueLabe1 = new Label();
-            valueLabe1.Text = "Last value";
-            valueLabe1.AutoSize = true;
-            valueLabe1.Location = new System.Drawing.Point(10, 10);
-            this.Controls.Add(valueLabe1);
 
         }
 
-        private void UpdateValueLabel(double value)
-        {
-            valueLabe1.Text = "Last value :" + value.ToString();
-        }
-
-        private void plotView1_MouseClick(object sender, MouseEventArgs e)
-        {
-            double lastvalue = series.Points[series.Points.Count - 1].Y;
-            UpdateValueLabel(lastvalue);
-        }
+        
 
         private void testchart_Load(object sender, EventArgs e)
         {
