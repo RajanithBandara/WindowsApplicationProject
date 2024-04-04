3db44c16d73b1b9061892a2de2160ae50629e7ff
Paranamana A O <aoparanamana@students.nsbm.ac.lk>
2024-04-04 14:38:44 +0530

    funtions control

---
+++ WindowsAppProject/Apps/usercontrol_progressiondash/Student by student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsAppProject.Apps.usercontrol_progressiondash
+{
+    public partial class Student_by_student : UserControl
+    {
+        public Student_by_student()
+        {
+            InitializeComponent();
+        }
+    }
+}
