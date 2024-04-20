132886908e9cdf23263473e743d8b8657c6e13db
Rajanith Bandara <RajanithBandara123@outlook.com>
2024-04-20 22:39:06 +0530

    changes

--- WindowsAppProject/Apps/all_student_content.Designer.cs
+++ WindowsAppProject/Apps/all_student_content.Designer.cs
@@ -45,6 +45,11 @@
             this.rjPanel1 = new RoundedPanelClass.RJPanel();
             this.label1 = new System.Windows.Forms.Label();
             this.rjPanel7 = new RoundedPanelClass.RJPanel();
+            this.StNaPanel = new System.Windows.Forms.Panel();
+            this.GPAPanel = new System.Windows.Forms.Panel();
+            this.CoNaPanel = new System.Windows.Forms.Panel();
+            this.CoIDPanel = new System.Windows.Forms.Panel();
+            this.StIDPanel = new System.Windows.Forms.Panel();
             this.BackPanel.SuspendLayout();
             this.rjPanel6.SuspendLayout();
             this.rjPanel5.SuspendLayout();
@@ -52,6 +57,7 @@
             this.rjPanel3.SuspendLayout();
             this.rjPanel2.SuspendLayout();
             this.rjPanel1.SuspendLayout();
+            this.rjPanel7.SuspendLayout();
             this.SuspendLayout();
             // 
             // BackPanel
@@ -265,6 +271,11 @@
             // 
             this.rjPanel7.BackColor = System.Drawing.Color.Green;
             this.rjPanel7.BorderRadius = 30;
+            this.rjPanel7.Controls.Add(this.StNaPanel);
+            this.rjPanel7.Controls.Add(this.GPAPanel);
+            this.rjPanel7.Controls.Add(this.CoNaPanel);
+            this.rjPanel7.Controls.Add(this.CoIDPanel);
+            this.rjPanel7.Controls.Add(this.StIDPanel);
             this.rjPanel7.ForeColor = System.Drawing.Color.White;
             this.rjPanel7.GradientAngle = 90F;
             this.rjPanel7.GradientBottomColor = System.Drawing.Color.Green;
@@ -273,6 +284,56 @@
             this.rjPanel7.Name = "rjPanel7";
             this.rjPanel7.Size = new System.Drawing.Size(1638, 813);
             this.rjPanel7.TabIndex = 2;
+            // 
+            // StNaPanel
+            // 
+            this.StNaPanel.AutoScroll = true;
+            this.StNaPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(60)))), ((int)(((byte)(80)))));
+            this.StNaPanel.Location = new System.Drawing.Point(221, 3);
+            this.StNaPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.StNaPanel.Name = "StNaPanel";
+            this.StNaPanel.Size = new System.Drawing.Size(645, 806);
+            this.StNaPanel.TabIndex = 15;
+            // 
+            // GPAPanel
+            // 
+            this.GPAPanel.AutoScroll = true;
+            this.GPAPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.GPAPanel.Location = new System.Drawing.Point(1420, 3);
+            this.GPAPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.GPAPanel.Name = "GPAPanel";
+            this.GPAPanel.Size = new System.Drawing.Size(205, 806);
+            this.GPAPanel.TabIndex = 14;
+            // 
+            // CoNaPanel
+            // 
+            this.CoNaPanel.AutoScroll = true;
+            this.CoNaPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(60)))), ((int)(((byte)(80)))));
+            this.CoNaPanel.Location = new System.Drawing.Point(1084, 3);
+            this.CoNaPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CoNaPanel.Name = "CoNaPanel";
+            this.CoNaPanel.Size = new System.Drawing.Size(330, 806);
+            this.CoNaPanel.TabIndex = 13;
+            // 
+            // CoIDPanel
+            // 
+            this.CoIDPanel.AutoScroll = true;
+            this.CoIDPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.CoIDPanel.Location = new System.Drawing.Point(872, 3);
+            this.CoIDPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.CoIDPanel.Name = "CoIDPanel";
+            this.CoIDPanel.Size = new System.Drawing.Size(205, 806);
+            this.CoIDPanel.TabIndex = 12;
+            // 
+            // StIDPanel
+            // 
+            this.StIDPanel.AutoScroll = true;
+            this.StIDPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.StIDPanel.Location = new System.Drawing.Point(14, 3);
+            this.StIDPanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.StIDPanel.Name = "StIDPanel";
+            this.StIDPanel.Size = new System.Drawing.Size(201, 806);
+            this.StIDPanel.TabIndex = 11;
             // 
             // all_student_content
             // 
@@ -305,6 +366,7 @@
             this.rjPanel2.PerformLayout();
             this.rjPanel1.ResumeLayout(false);
             this.rjPanel1.PerformLayout();
+            this.rjPanel7.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -328,5 +390,10 @@
         private RoundedPanelClass.RJPanel rjPanel2;
         private System.Windows.Forms.Label lblStID;
         private RoundedPanelClass.RJPanel rjPanel7;
+        private System.Windows.Forms.Panel StNaPanel;
+        private System.Windows.Forms.Panel GPAPanel;
+        private System.Windows.Forms.Panel CoNaPanel;
+        private System.Windows.Forms.Panel CoIDPanel;
+        private System.Windows.Forms.Panel StIDPanel;
     }
 }
